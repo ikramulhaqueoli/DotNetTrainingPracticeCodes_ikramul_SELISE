df46ef54d8c7c622308d646953d834a3d5c372b6
Ikramul Haque Chowdhury <ikramuloli@gmail.com>
2020-11-10 12:04:58 +0600

    Removed auto generated vs files

---
+++ ToDoAppUsingWebApi/Models/TodoTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAppUsingWebApi.Models
+{
+    public class TodoTask
+    {
+        public TodoTask()
+        {
+            AssignedEmployees = new List<Employee>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+        public DateTime DeadLine { get; set; }
+        public DateTime AssignDate { get; set; }
+
+        public ICollection<Employee> AssignedEmployees { private set; get; }
+    }
+}
---
+++ ToDoAppUsingWebApi/Repositories/TodoTaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoAppUsingWebApi.Models;
+
+namespace ToDoAppUsingWebApi.Repositories
+{
+    public class TodoTaskRepository : Repository<TodoTask>
+    {
+        public TodoTaskRepository()
+        {
+            this._appDbSet = _appDbContext.TodoTasks;
+        }
+
+        public static TodoTaskRepository Instance => new TodoTaskRepository();
+    }
+}
