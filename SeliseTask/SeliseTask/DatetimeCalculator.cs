781d173121660727a924579409fc2b552e4f0584
Ikramul Haque Chowdhury <ikramuloli@gmail.com>
2020-11-22 13:22:33 +0600

    Added a test method named LeapYearTest() in the Unit Test project

--- SeliseTask/SeliseTask/DatetimeCalculator.cs
+++ SeliseTask/SeliseTask/DatetimeCalculator.cs
@@ -12,5 +12,13 @@ namespace SeliseTask
             this._datetimeProvider = _datetimeProvider;
         }
         public int CurrentQuarter => (this._datetimeProvider.UtcNow.Month - 1) / 3 + 1;
+
+        public bool IsLeapYear()
+        {
+            return (
+                (this._datetimeProvider.UtcNow.Year % 100 != 0 &&
+                this._datetimeProvider.UtcNow.Year % 4 == 0) ||
+                this._datetimeProvider.UtcNow.Year % 400 == 0) ;
+        }
     }
 }
--- SeliseTask/TestLaboratory/DatetimeTester.cs
+++ SeliseTask/TestLaboratory/DatetimeTester.cs
@@ -25,5 +25,21 @@ namespace TestLaboratory
 
             Assert.AreEqual(dtprocessor.CurrentQuarter, 2);
         }
+
+        [TestMethod]
+        public void LeapYearTest()
+        {
+            UnityFactory.RegisterComponents();
+
+            var datetimeProviderMock = new Mock<IDatetimeProvider>();
+            datetimeProviderMock.SetupGet(dtp => dtp.UtcNow).Returns(new DateTime(2020, 6, 1));
+
+            UnityFactory.container.RegisterType<DatetimeCalculator, DatetimeCalculator>
+                (new InjectionConstructor(datetimeProviderMock.Object));
+
+            DatetimeCalculator dtprocessor = UnityFactory.container.Resolve<DatetimeCalculator>();
+
+            Assert.IsTrue(dtprocessor.IsLeapYear());
+        }
     }
 }
